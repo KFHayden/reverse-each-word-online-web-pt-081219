634612af5ce37755e702656b50e32283b3bad456
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-24 20:57:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   new_array = sentence1.split
-  reversed_array = new_array.each do |word|
+  reversed_array = new_array.map do |word|
     word.reverse
   end
 end
